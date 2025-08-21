2f5d9f920801fc014560976335398d56ecf8867b
H1ghtow3r <markus-stadler@live.de>
2025-08-21 23:52:51 +0200

    added function get_number_of_words

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+    
+
+def get_number_of_words(text):
+    words = text.split()
+    return len(words)
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_number_of_words(text)
+    print(f"{num_words} words found in the document")
+
+ 
+main()
